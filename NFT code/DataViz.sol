b9f31922539a35fa1321f4cdfcc1bbb1892b2c5e
SuperProgrammer775 <57545784+SuperProgrammer775@users.noreply.github.com>
2022-01-16 15:18:24 -0500

    Create DataViz.sol

---
+++ NFT code/DataViz.sol
@@ -0,0 +1,19 @@
+pragma solidity^0.8.7;
+
+// SPDX-License-Identifier: MIT
+// Created by Synthetic DAO
+
+contract DataViz {
+    address owner;
+
+    string public standard = "DataViz";
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    uint256 public initialSupply = 0.015 ether;
+
+    constructor() {
+        totalSupply = 8888;
+    }
+}
